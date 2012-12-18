5308b4285e2fbd2db5c8cd1275b141c481a60565
Puppetmaster <puppetmaster@avira.com>
2012-12-18 10:12:55 +0000

    added devtools to apc env.

--- files/devtools.sh
+++ files/devtools.sh
@@ -1,6 +1,8 @@
 export EDITOR=emacs
 
 alias g='git'
+alias gst='git status'
+alias gca='git commit -a'
 alias cdpierrot='cd /usr/local/src/avira.pierrot/usr/lib/pierrot/'
 alias pierrotup='cd /usr/local/src/avira.pierrot/usr/lib/pierrot/ && git pull --ff-only'
 alias puppetup='service puppet stop && puppet agent --no-daemonize --verbose --onetime && service puppet start'
